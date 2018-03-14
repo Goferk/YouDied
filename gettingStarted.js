ad8ba2ff7b94b550b81618f50d695c4dcaf7bd52
Javier Echiburu <goferk95@gmail.com>
2018-03-14 00:17:31 -0300

    Agregar turnos al juego

--- gettingStarted.js
+++ gettingStarted.js
@@ -39,43 +39,59 @@ Estado1.preload = function () {
 
 Estado1.create = function(){
 
-    Kiwi.State.prototype.create.call( this );
+  this.myButton = new Kiwi.HUD.Widget.Button( this.game, 'Finalizar Turno', 650, 25 );
+  this.game.huds.defaultHUD.addWidget( this.myButton );
 
-    this.background = new Kiwi.GameObjects.StaticImage( this, this.textures.background, 0, 0 );
-    this.p1 = new Kiwi.GameObjects.Sprite( this, this.textures.heroe1Sprite, 12*25, 12*25 );
-    this.p2 = new Kiwi.GameObjects.Sprite( this, this.textures.heroe2Sprite, 13*25, 12*25 );
-    this.p3 = new Kiwi.GameObjects.Sprite( this, this.textures.heroe3Sprite, 12*25, 13*25 );
-    this.p4 = new Kiwi.GameObjects.Sprite( this, this.textures.heroe4Sprite, 13*25, 13*25 );
-    this.b = new Kiwi.GameObjects.Sprite( this, this.textures.bossSprite, 25, 25 );
+  this.myButton.style.color = 'white';
+  this.myButton.style.fontSize = '2em';
+  this.myButton.style.fontWeight = 'bold';
+  this.myButton.style.padding = '0.5em 1em';
+  this.myButton.style.backgroundColor = 'black';
+  this.myButton.style.cursor = 'pointer';
 
-    for (let i = 1; i <= 24; i++) {
-      for (let j = 1; j <= 24; j++) {
-        this['tablero' + i.toString() + 'x' + j.toString()] = new Kiwi.GameObjects.Sprite( this, this.textures.blockSprite, 25*i, 25*j );
-      }
+  this.myButton.input.onDown.add( this.buttonPressed, this );
+  this.myButton.input.onUp.add( this.buttonReleased, this );
+
+  this.myButton.input.onOver.add( this.buttonOver, this );
+  this.myButton.input.onOut.add( this.buttonOut, this );
+
+  Kiwi.State.prototype.create.call( this );
+
+  this.background = new Kiwi.GameObjects.StaticImage( this, this.textures.background, 0, 0 );
+  this.p1 = new Kiwi.GameObjects.Sprite( this, this.textures.heroe1Sprite, 12*25, 12*25 );
+  this.p2 = new Kiwi.GameObjects.Sprite( this, this.textures.heroe2Sprite, 13*25, 12*25 );
+  this.p3 = new Kiwi.GameObjects.Sprite( this, this.textures.heroe3Sprite, 12*25, 13*25 );
+  this.p4 = new Kiwi.GameObjects.Sprite( this, this.textures.heroe4Sprite, 13*25, 13*25 );
+  this.b = new Kiwi.GameObjects.Sprite( this, this.textures.bossSprite, 25, 25 );
+
+  for (let i = 1; i <= 24; i++) {
+    for (let j = 1; j <= 24; j++) {
+      this['tablero' + i.toString() + 'x' + j.toString()] = new Kiwi.GameObjects.Sprite( this, this.textures.blockSprite, 25*i, 25*j );
     }
+  }
 
-    this.addChild( this.background );
+  this.addChild( this.background );
 
-    for (let i = 1; i <= 24; i++) {
-      for (let j = 1; j <= 24; j++) {
-        this['tablero' + i.toString() + 'x' + j.toString()].animation.add("normal", [0], 0.1, false);
-        this['tablero' + i.toString() + 'x' + j.toString()].animation.add("disponible", [1], 0.1, false);
-        this['tablero' + i.toString() + 'x' + j.toString()].animation.play("normal");
-        this['tablero' + i.toString() + 'x' + j.toString()].input.onUp.add(this.moverse, this);
-        this.addChild( this['tablero' + i.toString() + 'x' + j.toString()] );
-      }
+  for (let i = 1; i <= 24; i++) {
+    for (let j = 1; j <= 24; j++) {
+      this['tablero' + i.toString() + 'x' + j.toString()].animation.add("normal", [0], 0.1, false);
+      this['tablero' + i.toString() + 'x' + j.toString()].animation.add("disponible", [1], 0.1, false);
+      this['tablero' + i.toString() + 'x' + j.toString()].animation.play("normal");
+      this['tablero' + i.toString() + 'x' + j.toString()].input.onUp.add(this.moverse, this);
+      this.addChild( this['tablero' + i.toString() + 'x' + j.toString()] );
     }
-    this.p1.input.onUp.add(this.disponibilidad2, this);
-    this.p2.input.onUp.add(this.disponibilidad2, this);
-    this.p3.input.onUp.add(this.disponibilidad2, this);
-    this.p4.input.onUp.add(this.disponibilidad2, this);
-    this.b.input.onUp.add(this.disponibilidad1, this);
+  }
+  this.p1.input.onUp.add(this.disponibilidad2, this);
+  this.p2.input.onUp.add(this.disponibilidad2, this);
+  this.p3.input.onUp.add(this.disponibilidad2, this);
+  this.p4.input.onUp.add(this.disponibilidad2, this);
+  this.b.input.onUp.add(this.disponibilidad1, this);
 
-    this.addChild( this.p1 );
-    this.addChild( this.p2 );
-    this.addChild( this.p3 );
-    this.addChild( this.p4 );
-    this.addChild( this.b );
+  this.addChild( this.p1 );
+  this.addChild( this.p2 );
+  this.addChild( this.p3 );
+  this.addChild( this.p4 );
+  this.addChild( this.b );
 }
 
 Estado1.moverse = function () {
@@ -104,7 +120,6 @@ Estado1.moverse = function () {
     Juego[newX-1][newY-1] = Juego[turno.onSelectx-1][turno.onSelecty-1];
     Juego[turno.onSelectx-1][turno.onSelecty-1] = '0'
     turno.mov = false;
-    console.log(Juego, Juego[newX-1][newY-1])
     this[Juego[newX-1][newY-1]].x = newX * 25;
     this[Juego[newX-1][newY-1]].y = newY * 25;
     for (let i=1; i<=24; i++) {
@@ -155,11 +170,41 @@ Estado1.disponibilidad1 = function () {
   }
 }
 
+Estado1.buttonPressed = function() {
+  this.myButton.x = 653;
+  this.myButton.y = 28;
+}
 
+Estado1.buttonReleased = function() {
+  if (turno.status) {
+    turno.status = 0;
+    turno.numero += 1;
+    turno.mov = false;
+    turno.f1 = 4;
+    turno.f2 = 4;
+    turno.f3 = 5;
+  } else {
+    turno.status = 1;
+    turno.mov = false;
+    turno.f1 = 3;
+    turno.f2 = 4;
+    turno.f3 = 7;
+  }
+  this.myButton.x = 650;
+  this.myButton.y = 25;
+}
+
+Estado1.buttonOver = function() {
+  this.myButton.style.backgroundColor = 'green';
+}
+
+Estado1.buttonOut = function() {
+  this.myButton.style.backgroundColor = 'black';
+}
 
 Estado1.update = function() {
 
-    Kiwi.State.prototype.update.call( this );
+  Kiwi.State.prototype.update.call( this );
 
 
 }
